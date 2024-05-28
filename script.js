b5b216a663ad266255819424c8940a006107a743
Aleksey <116443908+AlekseykaWRY@users.noreply.github.com>
2024-05-28 21:08:40 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+// function setBodyMarginTop() {
+//     var headerHeight = document.querySelector('header').offsetHeight;
+//     document.body.style.marginTop = headerHeight * 1.1 +'px';
+// }
+// window.addEventListener('load', setBodyMarginTop);
+// window.addEventListener('resize', setBodyMarginTop);
+
+
+// window.addEventListener('scroll', function() {
+//     let value = window.scrollY;
+//     let greenYellowLines = document.querySelectorAll('.green_line, .yellow_line');
+//     let num = (0.0 + value * 0.006).toFixed(2);
+//     num = Math.min(Math.max(num, 0.0), 1);
+
+//     greenYellowLines.forEach(line => {
+//         line.style.transformOrigin = 'left center';
+//         line.style.transform = `scaleX(${num})`;
+//         line.style.opacity = '1';
+//     });
+// });
